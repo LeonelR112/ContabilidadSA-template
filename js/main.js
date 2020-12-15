5bd8674b54d3f3ddeedd2459a7961e1d9495a914
Leonel <Leonel.Ruiz112@gmail.com>
2020-12-15 16:27:22 -0300

    menu con seguimiento y fondo de presentación en el index animado

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ $(window).ready(function (){
     let botonCerrar = document.getElementById('botonCerrar')
 
     $(botonMenu).click(function(e){
-        console.log(e)
         e.preventDefault();
 
         menuResponsive.style.top = "0px";
@@ -18,4 +17,29 @@ $(window).ready(function (){
     })
 
      //---------------
+
+     //funciones con scroll
+
+    $(window).scroll(function (e){
+        
+        let scrollPositionTop = $(window).scrollTop()
+        let menu = document.getElementById('navBar');
+
+        if(scrollPositionTop >= 156){
+            menu.style.position = "fixed";
+            menu.style.top = "0px"
+            menu.style.zIndex = "65"
+        }
+        else{
+            menu.style.position = "relative";
+        }
+
+        let presentacion = document.getElementById('presentacion');
+
+        presentacion.style.backgroundPositionY = parseInt(scrollPositionTop / 3) + "%"
+
+    });
+
+     //-----------------
+
 })
